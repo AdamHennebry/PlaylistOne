c43b842bdf1b373f032cd11ca3812d610c0f0db5
Glitch (playlist-1-adamh) <none>
2018-02-07 11:48:20 +0000

    ⏲🛀 Checkpoint views/partials/listsongs.hbs:657844/16 views/dashboard.hbs:657844/3 controllers/dashboard.js:657844/39 models/playlist-store.js:657844/3

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,13 +1,16 @@
 'use strict';
 
 const logger = require('../utils/logger');
+const sonatas = require('../models/playlist-store.js');
 
 const dashboard = {
   index(request, response) {
     logger.info('dashboard rendering');
-       const viewData = {
+    const viewData = {
       title: 'Playlist Dashboard',
+      playlist: sonatas,
     };
+    logger.info('about to render', sonatas);
     response.render('dashboard', viewData);
   },
 };
